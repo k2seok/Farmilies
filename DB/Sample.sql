da067c03845e5f91dfe7b37cdd180611be04e6bc
fkalar747 <39028696+fkalar747@users.noreply.github.com>
2018-06-02 02:25:27 +0900

    Sample sql

--- DB/Sample.sql
+++ DB/Sample.sql
@@ -1,10 +1,35 @@
+
+insert into user_groups (id,name,update_at,create_at) values (101,'admins',
+TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'),
+TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss')); 
+
+insert into user_groups (id,name,update_at,create_at) values (102,'farmers',
+TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'),
+TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss')); 
+
+insert into user_groups (id,name,update_at,create_at) values (103,'workers',
+TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'),
+TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'));  
+
+insert into user_groups (id,name,update_at,create_at) values (104,'farmerworkers',
+TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'),
+TO_DATE('2003/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'));  
+
+
+
+insert into pictures(id,picname,pic_loc) values (001,'awbwab','aa/bb/cc'); 
+insert into pictures(id,picname,pic_loc) values (002,'ddawab','aa/bb/cc'); 
+insert into pictures(id,picname,pic_loc) values (003,'eeawbab','aa/bb/cc'); 
+insert into pictures(id,picname,pic_loc) values (004,'rgfawwab','aa/bb/cc'); 
+
+
 insert into USERS (ID,EMAIL,PASSWORD,FIRST_NAME,LAST_NAME,GROUP_ID,PICTURE_ID)
-values ('laggu','kwla103@naver.com','laggu','구원','라',1,123141);
+values ('laggu','kwla103@naver.com','laggu','구원','라',101,001);
 insert into USERS (ID,EMAIL,PASSWORD,FIRST_NAME,LAST_NAME,GROUP_ID,PICTURE_ID)
-values ('lsupertopl','lsupertopl@naver.com','lsupertopl','병준','김',2,313141);
+values ('lsupertopl','lsupertopl@naver.com','lsupertopl','병준','김',102,002);
 insert into USERS (ID,EMAIL,PASSWORD,FIRST_NAME,LAST_NAME,GROUP_ID,PICTURE_ID)
-values ('fkalar747','fkalar747@naver.com','fkalar747','남호','이',1,223141);
+values ('fkalar747','fkalar747@naver.com','fkalar747','남호','이',103,003);
 insert into USERS (ID,EMAIL,PASSWORD,FIRST_NAME,LAST_NAME,GROUP_ID,PICTURE_ID)
-values ('윤기석','윤기석@naver.com','윤기석','기석','윤',2,3141);
+values ('윤기석','윤기석@naver.com','윤기석','기석','윤',104,004);
 insert into USERS (ID,EMAIL,PASSWORD,FIRST_NAME,LAST_NAME,GROUP_ID,PICTURE_ID)
-values ('한나영','한나영@naver.com','한나영','나영','한',1,211141);
+values ('한나영','한나영@naver.com','한나영','나영','한',105,001);
