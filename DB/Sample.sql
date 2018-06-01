c6bd7304026440648569118e942acc9430fc9bca
pseudotop <lsupertopl@gmail.com>
2018-06-02 02:03:49 +0900

    Added Sql sample

---
+++ DB/Sample.sql
@@ -0,0 +1,10 @@
+insert into USERS (ID,EMAIL,PASSWORD,FIRST_NAME,LAST_NAME,GROUP_ID,PICTURE_ID)
+values ('laggu','kwla103@naver.com','laggu','구원','라',1,123141);
+insert into USERS (ID,EMAIL,PASSWORD,FIRST_NAME,LAST_NAME,GROUP_ID,PICTURE_ID)
+values ('lsupertopl','lsupertopl@naver.com','lsupertopl','병준','김',2,313141);
+insert into USERS (ID,EMAIL,PASSWORD,FIRST_NAME,LAST_NAME,GROUP_ID,PICTURE_ID)
+values ('fkalar747','fkalar747@naver.com','fkalar747','남호','이',1,223141);
+insert into USERS (ID,EMAIL,PASSWORD,FIRST_NAME,LAST_NAME,GROUP_ID,PICTURE_ID)
+values ('윤기석','윤기석@naver.com','윤기석','기석','윤',2,3141);
+insert into USERS (ID,EMAIL,PASSWORD,FIRST_NAME,LAST_NAME,GROUP_ID,PICTURE_ID)
+values ('한나영','한나영@naver.com','한나영','나영','한',1,211141);
